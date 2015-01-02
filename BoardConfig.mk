096e0d4f57bae6e611c04b29b5ffbbce765801b9
Dragon <dragon7780@hotmail.nl>
2015-01-02 17:40:10 +0100

    Compile Kernel with SaberMod 4.10
    Change-Id: I5a3ff1397fa42c7d3868b8c6c43623a6f25419bf
    Signed-off-by: Dragon <dragon7780@hotmail.nl>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,7 @@ TARGET_OTA_ASSERT_DEVICE := kltespr,klte
 
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_spr_defconfig
+TARGET_GCC_VERSION_ARM := 4.10-sm
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
